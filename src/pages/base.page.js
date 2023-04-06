241bfca8f7c011c58b013baf55ca30bb30f73665
Ghada Ayari <gayari@iobeya.com>
2023-04-06 04:05:15 +0200

    Add uploadFile function in the base page

--- src/pages/base.page.js
+++ src/pages/base.page.js
@@ -6,3 +6,8 @@ export default class BasePage {
     async navigate(url, path) {
         await this.page.goto(url + path);
     }
+
+    async uploadFile(selector, filePath) {
+        const inputElement = await this.page.$(selector);
+        await inputElement.setInputFiles(filePath);
+    }
