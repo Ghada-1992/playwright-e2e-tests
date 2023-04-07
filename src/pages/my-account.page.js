454e042b33305d624538c478f1d0d58281e829e8
Ghada Ayari <gayari@iobeya.com>
2023-04-07 19:22:55 +0200

    Add avatarInputFile selector

--- src/pages/my-account.page.js
+++ src/pages/my-account.page.js
@@ -4,6 +4,6 @@ export default class MyAccountPage extends BasePage {
     constructor(page) {
         super(page);
 
-        this.editAvatar = '[data-testid="account-edit-field-avatar"]';
+        this.avatarInputFile = '#avatar input[type="file"]';
     }
 }
