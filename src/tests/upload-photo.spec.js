43448f4bcff62008afdd51cf5e5ff996cff8ce4f
Ghada Ayari <gayari@iobeya.com>
2023-04-07 19:23:59 +0200

    Update upload profile photo test scenario

--- src/tests/upload-photo.spec.js
+++ src/tests/upload-photo.spec.js
@@ -2,25 +2,33 @@ import { expect } from '@playwright/test';
 
 import BasePage from '../pages/base.page';
 import LoginPage from '../pages/login.page';
+import UserMenuPage from '../pages/user-menu.page';
+import MyAccountPage from '../pages/my-account.page';
 
 describe('Photo upload tests', () => {
-    let context, page, basePage, loginPage;
+    let context, page, basePage, loginPage, userMenuPage, myAccountPage;
 
     beforeEach(async () => {
         context = await browser.newContext();
         page = await context.newPage();
         basePage = new BasePage(page);
         loginPage = new LoginPage(page);
+        userMenuPage = new UserMenuPage(page);
+        myAccountPage = new MyAccountPage(page);
     });
 
     afterEach(async () => {
-        await context.close();
+        //await context.close();
     });
 
     it('Should upload a profile photo after signing-in with correct credentials', async () => {
-        await basePage.navigate("https://www.welcometothejungle.com/fr/me/settings/account");
-        await loginPage.enterEmail("qaautomationapplicant@gmail.com");
-        await loginPage.enterPassword("o5N,d5ZR@R7^");
+        await basePage.navigate('https://www.welcometothejungle.com/fr/me/settings/account');
+        await loginPage.enterEmail('inqom.qaautomationapplicant@gmail.com');
+        await loginPage.enterPassword('o5N,d5ZR@R7^');
         await loginPage.clickSubmitButton();
+        await expect(page.locator(userMenuPage.userMenuLinks)).toBeVisible();
+        await expect(page.locator(userMenuPage.userMenuLinkSettings)).toHaveAttribute('aria-selected', 'true');
+
+        await basePage.uploadFile(myAccountPage.avatarInputFile, 'profile-photo.png');
     });
 });
