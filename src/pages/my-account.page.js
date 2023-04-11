5fbd3195a349ad5e4a8ff428cc448bdec28e6323
Ghada Ayari <gayari@iobeya.com>
2023-04-11 05:38:40 +0200

    Update my account page

--- src/pages/my-account.page.js
+++ src/pages/my-account.page.js
@@ -5,5 +5,11 @@ export default class MyAccountPage extends BasePage {
         super(page);
 
         this.avatarInputFile = '#avatar input[type="file"]';
+        this.editAvatarBtn = 'button [alt="Edit"]';
+        this.editAccountSubmitBtn = '[data-testid="account-edit-button-submit"]';
+    }
+
+    async clickEditAccountSubmitBtn() {
+        await this.page.click(this.editAccountSubmitBtn);
     }
 }
