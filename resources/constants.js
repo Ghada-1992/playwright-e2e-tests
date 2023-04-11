76676cfa535d02bb328154f1f312d1ce5af4d373
Ghada Ayari <gayari@iobeya.com>
2023-04-11 06:14:35 +0200

    Add constants file

---
+++ resources/constants.js
@@ -0,0 +1,3 @@
+export const my_account_url_path = 'me/settings/account';
+export const notifications_my_account_update_text_fr =
+    'Mise à jour réussie !Vos informations personnelles ont bien été mises à jour.';
